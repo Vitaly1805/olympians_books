cc3920003a0b564ec2eef3c43f2cc122ee51b5fe
Nosov Vitaly <vitalinosov2000@gmail.com>
2023-02-12 19:54:00 +0300

    Adaptet the header

--- src/components/header/v-header.vue
+++ src/components/header/v-header.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="v-header">
+  <div class="v-header"
+    :class="{active: isActive}">
     <div class="v-header__body _container">
-      <div class="v-header__name">
-        <span>Olympians</span> 
-        <span>Books</span>
+      <vMenu 
+        :isActive="isActive"/>
+      <div class="v-header__top">
+        <div class="v-header__name">
+          <span>Olympians</span> 
+          <span>Books</span>
+        </div>
+        <div
+          class="v-header__arrow"
+          @click="toggleActive"></div>
       </div>
-      <div class="v-header__arrow"></div>
       <div class="v-header__title">
         Книги, которые вознесут вас на Олимп
       </div>
@@ -15,17 +22,65 @@
 
 <script>
 
+import vMenu from './../menu/v-menu.vue'
+
 export default {
-  name: 'v-header'
+  name: 'v-header',
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  components: {
+    vMenu
+  },
+  methods: {
+    toggleActive() {
+      this.isActive = !this.isActive
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .v-header {
   height: 940px;
-  background: url(../../assets/images/header/back.png) no-repeat;
+  background: url(../../assets/images/header/back.png) center center;
   background-size: cover;
-  position: relative;
+
+  @media (max-width: $mobile) {
+    height: 100vh;
+    background: #1399c3 url(../../assets/images/header/back-mobile.png) left bottom no-repeat;
+    background-size: contain;
+  }
+
+  &.active {
+    .v-header__arrow {
+      &::before,
+      &::after {
+        background: #000;
+      }
+
+      &::before {
+        left: 28%;
+        transform: translateX(-50%) rotate(150deg);
+      }
+
+      &::after {
+        transform: translateX(-50%) rotate(-150deg);
+      }
+    }
+
+    @media (max-width: $mobile) {
+      .v-header__name {
+        color: #000;
+      }
+    }
+  }
+
+  &__top {
+    position: relative;
+  }
 
   &__name {
     font-family: 'Archeologicaps';
@@ -34,28 +89,67 @@ export default {
     line-height: 36px;
     display: flex;
     flex-direction: column;
+    transition: all .5s ease;
+
+    @media (max-width: $mobile) {
+      text-align: center;
+    }
   }
 
   &__arrow {
+    position: absolute;
+    cursor: pointer;
+    width: 64px;
+    height: 18px;
+    top: 41px;
+    left: 50%;
+    transform: translateX(-50%);
+
+    @media (max-width: $mobile) {
+      top: 120px;
+    }
+
     &::before,
     &::after {
       content: '';
+      transition: all .5s ease;
       position: absolute;
-      height: 20px;
-      width: 20px;
+      height: 3px;
+      width: 56%;
       background: #fff;
-      top: 20px;
-      left: 20px;
+      bottom: 8px;
+      border-radius: 5px;
+    }
+
+    &::before {
+      left: 27%;
+      transform: translateX(-50%) rotate(25deg);
+    }
+
+    &::after {
+      left: 73%;
+      transform: translateX(-50%) rotate(-25deg);
     }
   }
 
   &__title {
     font-family: 'Forum';
-    font-size: 80px;
-    line-height: 120px;
+    @include adaptive-value('font-size', 80, 30, 1);
+    @include adaptive-value('line-height', 120, 50, 1);
     letter-spacing: 0.07em;
     text-transform: uppercase;
     max-width: 860px;
+    width: 80%;
+
+    @media (max-width: $mobile) {
+      letter-spacing: 0.02em;
+      width: 100%;
+      margin: 80px 0 0 0;
+    }
+
+    @media (max-width: 600px) {
+      text-align: center; 
+    }
   }
 }
 </style>
---
+++ src/components/menu/v-menu.vue
@@ -0,0 +1,86 @@
+<template>
+  <div 
+    class="v-menu"
+    :class="{active: isActive}">
+    <div class="v-menu__body _container">
+      <div class="v-menu__list">
+        <div 
+          class="v-menu__item"
+          v-for="item of menuItems"
+          :key="item.id">
+          {{ item.name }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'v-menu',
+  props: {
+    isActive() {
+      return {
+        type: Boolean,
+        default: false
+      }
+    }
+  },
+  data() {
+    return {
+      menuItems: [
+        {
+          id: 1,
+          name: 'Главная',
+          link: ''
+        },
+        {
+          id: 2,
+          name: 'Каталог',
+          link: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.v-menu {
+  max-width: 420px;
+  width: 100%;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -100%);
+  color: #000;
+  transition: all .5s ease;
+
+  &.active {
+    transform: translate(-50%, 0);
+  }
+
+  &__list {
+    padding: 120px 0 54px 0;
+    font-family: 'Forum';
+    display: flex;
+    flex-direction: column;
+    gap: 56px;
+
+    @media (max-width: $mobile) {
+      padding: 150px 0 54px 0;
+      gap: 26px;
+    }
+  }
+
+  &__item {
+    cursor: pointer;
+    text-align: center;
+    letter-spacing: 0.07em;
+    font-size: 30px;
+    line-height: 45px;
+  }
+}
+</style>
